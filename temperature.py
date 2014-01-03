0d7013bd6e1a028fb37db3ad23173627854d6711
cptdeadbones <cpt@thelivingpearl.com>
2014-01-02 20:20:51 -0500

    Create temperature.py
    initial commit

---
+++ temperature.py
@@ -0,0 +1,17 @@
+def c2f(t):
+	return (t*9/5.0)+32
+
+def c2k(t):
+	return t+273.15
+
+def f2c(t):
+	return (t-32)*5.0/9
+
+def f2k(t):
+	return (t+459.67)*5.0/9
+
+def k2c(t):
+	return t-273.15
+
+def k2f(t):
+	return (t*9/5.0)-459.67
